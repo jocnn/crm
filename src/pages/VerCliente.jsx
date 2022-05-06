41cbbb9e345c8b540ae1fb1ed7cee9d66f93f34e
jocnn <ijann_@outlook.com>
2022-05-06 20:38:35 -0300

    agregando un spinner de carga

--- src/pages/VerCliente.jsx
+++ src/pages/VerCliente.jsx
@@ -1,6 +1,8 @@
 import { useState, useEffect } from 'react'
 import { useParams } from "react-router-dom"
 
+import Spinner from "../components/Spinner"
+
 const VerCliente = () => {
   
   const { id } = useParams()
@@ -15,65 +17,69 @@ const VerCliente = () => {
         const url = `http://localhost:4000/clientes/${id}`
         const respuesta = await fetch(url)
         const resultado = await respuesta.json()
-        
+
         setCliente(resultado)
         
       } catch (error) {
         console.log(error)
       }
-      setCargando(false)
+      
+      setTimeout(() => {
+        setCargando(false)
+      }, 3000);
     }
 
     obtenerClienteAPI()
   }, [])
 
   return (
+    cargando ? 
+      <Spinner /> : 
+      Object.keys(cliente).length === 0 ? <p>No hay Resultados</p> : (
+        <div>
+          {
+            cargando ? 'cargando...' : (
+              <>          
+                <h1 className='font-black text-4xl text-blue-900'>Ver Cliente: {cliente.nombre}</h1>
+                <p className='mt-3'>Informacion del Cliente</p>
 
-    Object.keys(cliente).length === 0 ? <p>No hay Resultados</p> : (
-      <div>
-        {
-          cargando ? 'cargando...' : (
-            <>          
-              <h1 className='font-black text-4xl text-blue-900'>Ver Cliente: {cliente.nombre}</h1>
-              <p className='mt-3'>Informacion del Cliente</p>
+                <p className='text-4xl text-gray-600 mt-10'>
+                  <span className='text-gray-800 uppercase font-bold'>Cliente: </span>
+                  {cliente.nombre}
+                </p>
 
-              <p className='text-4xl text-gray-600 mt-10'>
-                <span className='text-gray-800 uppercase font-bold'>Cliente: </span>
-                {cliente.nombre}
-              </p>
+                <p className='text-2xl text-gray-600 mt-4'>
+                  <span className='text-gray-800 uppercase font-bold'>Empresa: </span>
+                  {cliente.empresa}
+                </p>
 
-              <p className='text-2xl text-gray-600 mt-4'>
-                <span className='text-gray-800 uppercase font-bold'>Empresa: </span>
-                {cliente.empresa}
-              </p>
+                <p className='text-2xl text-gray-600 mt-4'>
+                  <span className='text-gray-800 uppercase font-bold'>Email: </span>
+                  {cliente.email}
+                </p>
 
-              <p className='text-2xl text-gray-600 mt-4'>
-                <span className='text-gray-800 uppercase font-bold'>Email: </span>
-                {cliente.email}
-              </p>
+                {
+                  cliente.telefono && (
+                    <p className='text-2xl text-gray-600 mt-4'>
+                      <span className='text-gray-800 uppercase font-bold'>Telefono: </span>
+                      {cliente.telefono}
+                    </p>
+                  )
+                }
 
-              {
-                cliente.telefono && (
-                  <p className='text-2xl text-gray-600 mt-4'>
-                    <span className='text-gray-800 uppercase font-bold'>Telefono: </span>
-                    {cliente.telefono}
-                  </p>
-                )
-              }
-
-              {
-                cliente.notas && (
-                  <p className='text-2xl text-gray-600 mt-4'>
-                    <span className='text-gray-800 uppercase font-bold'>Notas: </span>
-                    {cliente.notas}
-                  </p>
-                )
-              }
-            </>
-          )
-        }
-      </div>
-    )
+                {
+                  cliente.notas && (
+                    <p className='text-2xl text-gray-600 mt-4'>
+                      <span className='text-gray-800 uppercase font-bold'>Notas: </span>
+                      {cliente.notas}
+                    </p>
+                  )
+                }
+              </>
+            )
+          }
+        </div>
+      )
   )
 }
 
